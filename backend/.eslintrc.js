f34c9126e1fb1f42ef2a5c6aa711bf81f67eb2ab
Choi Yeonung <yonsweng@gmail.com>
2022-10-07 09:52:59 +0900

    Change backend to node.js with typescript

---
+++ backend/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+}
