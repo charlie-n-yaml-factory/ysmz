40e4597ed83b8c5588cf562ede98808fb22dc61c
Choi Yeonung <yonsweng@gmail.com>
2022-10-29 17:27:00 +0900

    Create iOS project

---
+++ ysmz-ios/ysmz-iosUITests/ysmz_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ysmz_iosUITestsLaunchTests.swift
+//  ysmz-iosUITests
+//
+//  Created by 최연웅 on 2022/10/29.
+//
+
+import XCTest
+
+final class ysmz_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
