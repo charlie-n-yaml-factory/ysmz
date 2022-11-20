ed8edff957e613202949e9c9540db3e420da8317
Choi Yeonung <39218496+yonsweng@users.noreply.github.com>
2022-11-20 10:08:01 +0900

    Create iOS project (#15)

---
+++ ysmz-ios/ysmz-ios/ysmz_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ysmz_iosApp.swift
+//  ysmz-ios
+//
+//  Created by 최연웅 on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct ysmz_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
